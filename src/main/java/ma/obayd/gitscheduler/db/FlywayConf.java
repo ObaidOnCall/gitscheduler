fdc61cbcba582bdfd11ccc117b87d5a7aa74dcda
Sof-Engine-Hour <abdellatif.hour.web@gmail.com>
2024-08-27 10:33:54 +0100

    Flyway can init its datasource form application.prop

--- src/main/java/ma/obayd/gitscheduler/db/FlywayConf.java
+++ src/main/java/ma/obayd/gitscheduler/db/FlywayConf.java
@@ -5,29 +5,46 @@ import org.springframework.context.annotation.Configuration;
 import org.flywaydb.core.Flyway;
 // import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 
 @Configuration
 public class FlywayConf {
 
+    private static Properties loadProperties() throws Exception {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = "src/main/resources/application.properties";
+        // String catalogConfigPath = rootPath + "catalog";
+
+        Properties appProps = new Properties();
+        appProps.load(new FileInputStream(appConfigPath));
+
+        // Properties catalogProps = new Properties();
+        // catalogProps.load(new FileInputStream(catalogConfigPath));
+
+        return  appProps ;
+    }
+
     /**
      * config the flyway
      * @return
+     * @throws Exception 
      */
     @Bean
-    public static Flyway flyway() {
+    public static Flyway flyway() throws Exception {
+        Properties properties = loadProperties();
         DataSource dataSource = DataSourceBuilder.create()
-                                .url("jdbc:mysql://localhost:3306/gitschedulerDB")
-                                .username("root")
-                                .password("Hour123*/*@@")
-                                .driverClassName("com.mysql.cj.jdbc.Driver")
+                                .url(properties.getProperty("spring.datasource.url"))
+                                .username(properties.getProperty("spring.datasource.username"))
+                                .password(properties.getProperty("spring.datasource.password"))
+                                .driverClassName(properties.getProperty("spring.datasource.driver-class-name"))
                                 .type(org.apache.tomcat.jdbc.pool.DataSource.class)
                                 .build();
-        // FluentConfiguration flywayConf = new FluentConfiguration();
-        // flywayConf.locations("filesystem:/migrations");
-        // flywayConf.dataSource(dataSource);
-        // flyway.setSchemas("my_schema");
         Flyway flyway = new Flyway(Flyway.configure().dataSource(dataSource).locations("classpath:db/migrations").baselineOnMigrate(true)) ;
         return flyway;
     }
--- src/main/java/ma/obayd/gitscheduler/db/FlywayRun.java
+++ src/main/java/ma/obayd/gitscheduler/db/FlywayRun.java
@@ -1,13 +1,23 @@
 package ma.obayd.gitscheduler.db;
 
+import org.flywaydb.core.api.FlywayException;
+
 // import ma.obayd.gitscheduler.db.FlywayConf;
 
 public class FlywayRun {
 
 
-    public static void migrate () {
+    public static void migrate (){
 
-        FlywayConf.flyway().migrate() ;
+        try {
+            FlywayConf.flyway().migrate() ;
+            System.out.println("migrated ✔️\n");
+        } catch (FlywayException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 
